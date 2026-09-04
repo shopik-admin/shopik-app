1a6f2f369eb962aea9adc7b79109a56e844b6103
yossefg <118802195+yossefgw@users.noreply.github.com>
2026-09-04 11:43:36 +0300

    WIP

--- client/pages/Home/index.jsx
+++ client/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import Button from 'common/components/Button'
 import events from 'common/features/events'
 import styles from './home.module.css'
+import Text from '#common/components/Text/index.jsx'
 
 export default function Home({ }) {
     return <div className={styles.home}>
-        Home
-        <Button onClick={() => events.emit('login-popover')}>login</Button>
+        <Text center bold size='h1'>Shopik Home</Text>
     </div>
 }
 
